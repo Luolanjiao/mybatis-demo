ad37adc9b5fc4c3d9e4125bb7cc5522a22fdf61e
wukangjie <897288110@qq.com>
2019-07-06 17:20:09 +0800

    注释

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.User;
-import com.sun.org.apache.bcel.internal.generic.RETURN;
+//import com.sun.org.apache.bcel.internal.generic.RETURN;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
